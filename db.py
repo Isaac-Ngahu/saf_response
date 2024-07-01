1efea5e4b2f894bcd170779c58eaf4227db1451e
Isaac-Ngahu <isaac.ngahu@student.moringaschool.com>
2024-07-01 12:41:06 +0300

    changing name of table in database

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ mydb = psycopg2.connect(
 def insert_response(sender,response):
   cursor = mydb.cursor()
   try:
-    sql = "INSERT INTO response (sender,reponse) VALUES (%s,%s)"
+    sql = "INSERT INTO saf_response (sender,reponse) VALUES (%s,%s)"
     values = (sender,response)
     cursor.execute(sql,values)
     mydb.commit()
@@ -17,7 +17,7 @@ def insert_response(sender,response):
 def get_responses():
   cursor = mydb.cursor()
   try:
-    sql = "SELECT * FROM response ORDER BY TimeReceived DESC LIMIT 3"
+    sql = "SELECT * FROM saf_response ORDER BY TimeReceived DESC LIMIT 3"
     cursor.execute(sql)
     rows = cursor.fetchall()
     return rows
